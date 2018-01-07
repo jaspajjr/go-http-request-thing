ad8f08d9a5200d0be7938f3b61f877465f72f146
John Carney <jaspajjr@gmail.com>
2018-01-07 01:15:08 +0000

    Updated container-ntrypoiny

--- container-entrypoint.sh
+++ container-entrypoint.sh
@@ -4,7 +4,7 @@ PATH=$PATH:/usr/sbin
 set -e
 
 if [ "$1" = "foo" ]; then
-  exec go run /go/src/http-thing/src/main.go
+  exec go run /go/src/http-thing/main.go
 elif [ "$1" = "-t" ]; then
-  exec py.test -m src
+  /bin/bash
 fi
